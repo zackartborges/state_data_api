ad49c2376eba8dd39f42e5d1000de483f795c45c
Zack Borges <zackartborges@gmail.com>
2021-06-15 09:36:33 -0400

    adds csv files

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to express' });
 });
+
 
 module.exports = router;
